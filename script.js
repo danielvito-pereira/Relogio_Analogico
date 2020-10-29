942c8c8727b8e402d9c9362aeaae3edcce576e48
Daniel Vitor Pereira da Silva <64044840+danielvito-pereira@users.noreply.github.com>
2020-10-29 11:58:09 -0300

    12

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ const PONTEIROHORA = document.querySelector("#hour");
 const PONTEIROMINUTO = document.querySelector("#minute");
 const PONTEIROSEGUNDO = document.querySelector("#second");
 
-
+function executarRelogio() {
 var data = new Date();
 let hr = data.getHours();
 let min = data.getMinutes();
 let seg = data.getSeconds();
 console.log("Hora: "+hr+" Minuto: "+min+" Segundo: "+ seg);
 
-let posicaoHr = hr*360/12+(min*360/60(/12));
+let posicaoHr = (hr*360/12)+(min*(360/60)/12);
 let posicaoMin = (min*360/60)+(seg*(360/60)/60);
 let posicaoSeg = seg*360/60;
 
@@ -17,3 +17,7 @@ let posicaoSeg = seg*360/60;
 PONTEIROHORA.style.transform = "rotate("+posicaoHr+"deg)";//foi aplicado transform ao estilo da constante com uma rotação que foi retirada da variavel posição hora no caso
 PONTEIROMINUTO.style.transform="rotate("+posicaoMin+"deg)";
 PONTEIROSEGUNDO.style.transform="rotate("+posicaoSeg+"deg)";
+
+
+}
+var intervalo =setInterval(executarRelogio,1000);//ser interval repete a fução segundo o intervalo definido 
