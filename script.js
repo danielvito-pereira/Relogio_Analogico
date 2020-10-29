9a129e6862c5f46d36e0a5baf31de0d17f35d2ba
Daniel Vitor Pereira da Silva <64044840+danielvito-pereira@users.noreply.github.com>
2020-10-29 11:50:55 -0300

    minutos segundos e horas

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ const PONTEIROMINUTO = document.querySelector("#minute");
 const PONTEIROSEGUNDO = document.querySelector("#second");
 
 
-let posicaoHr = 24;
-let posicaoMin = 130;
-let posicaoSeg = 267;
+var data = new Date();
+let hr = data.getHours();
+let min = data.getMinutes();
+let seg = data.getSeconds();
+console.log("Hora: "+hr+" Minuto: "+min+" Segundo: "+ seg);
+
+let posicaoHr = hr*360/12+(min*360/60(/12));
+let posicaoMin = (min*360/60)+(seg*(360/60)/60);
+let posicaoSeg = seg*360/60;
 
 
 PONTEIROHORA.style.transform = "rotate("+posicaoHr+"deg)";//foi aplicado transform ao estilo da constante com uma rotação que foi retirada da variavel posição hora no caso
